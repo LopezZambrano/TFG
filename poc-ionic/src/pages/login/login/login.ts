0659a149260e64213b9c6eefd6e58d9699ca3739
slopezzambrano <szambrab@gmail.com>
2017-05-23 18:00:05 +0200

    poll

--- poc-ionic/src/pages/login/login/login.ts
+++ poc-ionic/src/pages/login/login/login.ts
@@ -37,6 +37,4 @@ export class LoginPage {
     this.nav.setRoot(RegisterPage); 
   }
 
- 
-
 } 
--- poc-ionic/src/pages/poll/poll.ts
+++ poc-ionic/src/pages/poll/poll.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import {User} from '../../object/user'
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'page-poll',
   templateUrl: 'poll.html'
 })
+
 export class PollPage {
 
-  user =  {email: 'pepe@gmail.com',
-           password: '1234',
-           foto: '1',
-           name: 'Pepe'}
+    controltype = new FormControl();
+    controldate = new FormControl();
+    type : string;
+    fecha : boolean = false;
+    date : string[]=[];
+    cont : boolean = false;
+
+    delete(dat){
+      var index = this.date.indexOf(dat);
+      if (index > -1) {
+        this.date.splice(index, 1);
+      }
+    }
+
+    mostrarFechas(){
+      this.controldate.valueChanges
+            .catch((err) => {
+                return [];
+            })
+            .subscribe((date:string) => {
+                  this.date.push(date);
+                  this.cont = true;
+                }
+            )}
+
+            
+    
+
+    ionViewDidEnter(){
+        this.controltype.valueChanges
+            .catch((err) => {
+                return [];
+            })
+            .subscribe((type:string) => {
+                this.type = type;
+                if (this.type == "fecha") {
+                  this.fecha = true;
+                  this.mostrarFechas();
+                } else {
+                  this.fecha = false;
+              }
+            });
+
+
+
+
+    }
+          
 }
